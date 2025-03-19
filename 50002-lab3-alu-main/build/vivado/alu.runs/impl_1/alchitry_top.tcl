d9df7b165bdd9ba0c07947676014b3539c7590ff
lolkabash <lolkabash@gmail.com>
2025-03-19 09:52:01 +0800

    fix: updated max test cases in auto_tester and build

--- 50002-lab3-alu-main/build/vivado/alu.runs/impl_1/alchitry_top.tcl
+++ 50002-lab3-alu-main/build/vivado/alu.runs/impl_1/alchitry_top.tcl
@@ -104,8 +104,8 @@ start_step init_design
 set ACTIVE_STEP init_design
 set rc [catch {
   create_msg_db init_design.pb
-  set_param chipscope.maxJobs 1
-  set_param runs.launchOptions { -jobs 4  }
+  set_param chipscope.maxJobs 4
+  set_param runs.launchOptions { -jobs 16  }
 OPTRACE "create in-memory project" START { }
   create_project -in_memory -part xc7a35tftg256-1
   set_property design_mode GateLvl [current_fileset]
